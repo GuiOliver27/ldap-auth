0f9414121baec5aba09d7696a3e47e07d3070774
Guilherme Monção <guilherme_oliveira27@outlook.com>
2025-08-03 19:43:10 -0400

    Feat: Implementação do SecurityConfig

---
+++ src/main/java/br/com/ldap/auth/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package br.com.ldap.auth.config;
+
+import org.springframework.security.config.Customizer;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	      .authorizeHttpRequests((authorize) -> authorize
+	        .anyRequest().fullyAuthenticated()
+	      )
+	      .formLogin(Customizer.withDefaults());
+
+	    return http.build();
+	  }
+	
+	@Bean
+	public LdapTemplate ldapTemplate() {
+		return new LdapTemplate(contextSource());
+	}
+	
+	@Bean
+	public LdapContextSource contextSource() {
+		LdapContextSource ldapContextSource = new LdapContextSource();
+		ldapContextSource.setUrl("ldap://localhost:10389");
+		ldapContextSource.setUserDn("uid=admin,ou=system");
+		ldapContextSource.setPassword("password");
+		return ldapContextSource;
+	}
+}
