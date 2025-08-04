4d9056374ea54b5431f36fab116209c64158c6ca
Guilherme Monção <guilherme_oliveira27@outlook.com>
2025-08-03 22:03:18 -0400

    Feat: Novas funcionalidades e correções de bugs

--- src/main/java/br/com/ldap/auth/controller/UserController.java
+++ src/main/java/br/com/ldap/auth/controller/UserController.java
@@ -1,5 +1,35 @@
 package br.com.ldap.auth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.ldap.auth.model.LdapUser;
+import br.com.ldap.auth.service.LdapService;
+
+@Controller
 public class UserController {
 
+	@Autowired
+	private LdapService ldapService;
+	
+	@GetMapping("/addUserForm")
+	public String addUserForm( Model model) {
+		model.addAttribute("ldapUser", new LdapUser());
+		return "addUser";
+	}
+	
+	@PostMapping("/addUser")
+	public String addUser( LdapUser ldapUser) {
+		ldapService.addUser(ldapUser);
+		return "success";
+	}
+	
+	@GetMapping("/userList")
+	public String userList( Model model) {
+		model.addAttribute("userList", ldapService.getAllUsers());
+		return "userList";
+	}
 }
--- src/main/java/br/com/ldap/auth/service/LdapService.java
+++ src/main/java/br/com/ldap/auth/service/LdapService.java
@@ -21,4 +21,9 @@ public class LdapService {
 		
 	}
 
+	public void addUser(LdapUser ldapUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
