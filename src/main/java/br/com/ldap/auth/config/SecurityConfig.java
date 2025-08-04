52a4141b599caa9ec99a1ca9cfe16c0824e832de
Guilherme Monção <guilherme_oliveira27@outlook.com>
2025-08-03 21:30:30 -0400

    Feat: Implementação do AuthenticationManager

--- src/main/java/br/com/ldap/auth/config/SecurityConfig.java
+++ src/main/java/br/com/ldap/auth/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package br.com.ldap.auth.config;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -38,5 +41,12 @@ public class SecurityConfig {
 		ldapContextSource.setUserDn("uid=admin,ou=system");
 		ldapContextSource.setPassword("password");
 		return ldapContextSource;
+	}
+	
+	@Bean
+	AuthenticationManager authManager(BaseLdapPathContextSource source) {
+			LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(source);
+			factory.setUserDnPatterns("cn={0},ou=users,ou=system");
+			return factory.createAuthenticationManager();
 	}
 }
